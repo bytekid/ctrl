4962150643122867b0def886bf6a475f9cc1f119
Sarah Winkler <sarah.winkler@uibk.ac.at>
2017-05-09 16:30:30 +0200

    move some code around to not extend already found loops

--- src/termination/src/loop.ml
+++ src/termination/src/loop.ml
@@ -60,7 +60,7 @@ let constraints_to_string = function
     [] -> ""
   | [c] -> P.to_string_term c
   | c :: cs ->
-    let add_constr str c = " /\\ " ^ (P.to_string_term c) in
+    let add_constr str c = (P.to_string_term c) ^ " /\\ " str in
     List.fold_left add_constr (P.to_string_term c) cs
 ;;
 
@@ -70,70 +70,6 @@ let explanation ((rl,_,_) as loop) =
   "We use the loop processor to conclude nontermination.\n" ^
   "The loop is given by the sequence \n" ^ (sequence_to_string loop) ^ "\n" ^
   (if cs = [] then "" else " with constraints " ^ (constraints_to_string cs))
-;;
-
-(* Shorthand to rename a rule. *)
-let rename_rule c rule =
-  let fn = Alph.fun_names c.alph in
-  let newenv = Environment.empty 10 in
-  Rule.rename rule c.env
-;;
-
-(* Narrow last term in sequence using given rule at position p. *)
-let narrow c ((st, rs,sigma) as seq) p rule =
-  let rule' = rename_rule c rule in
-  let l,r = Rule.lhs rule', Rule.rhs rule' in
-  try
-    let s,t = Rule.lhs st, Rule.rhs st in
-    let mgu = Elogic.unify (Term.subterm p t) l in
-    let constr = Rule.constraints st @ (Rule.constraints rule') in
-    let st' = Rule.apply_sub mgu (Rule.create s (Term.replace p r t) constr) in
-    let sigma' = Sub.compose Sub.apply_term sigma mgu in
-    [st', rs @ [(rule, p)], sigma']
-  with Elogic.Not_unifiable -> []
-;;
-
-(* Do forward narrowing using rules in trs from last terms in sequence, trying
-   all possible rules and positions. *)
-let forward c (rules_root, rules_below) ((st,rs,_) as seq) =
-  let rlps_root = List.map (Util.Pair.make Pos.root) rules_root in
-  let ps = L.remove Pos.root (Term.funs_pos (Rule.rhs st)) in
-  let rlps = rlps_root @ (L.product ps rules_below) in
-  LL.of_list (L.flat_map (fun (p,rl) -> narrow c seq p rl) rlps)
-;;
-
-(* Checking whether the sequence does not exceed the maximal term size. *)
-let small c (st,_,_) =
-  Term.size (Rule.lhs st) <= c.max_size && Term.size (Rule.lhs st) <= c.max_size
-;;
-
-(* Checking whether the sequence does not exceed the maximal length. *)
-let short c (_,rs,_) = List.length rs < c.max_length
-
-(* Check whether constraints cs are satisfiable. *)
-let constr_sat ctxt cs =
-  let mk_fun = Term.make_function ctxt.alph ctxt.env in
-  let conj = Alph.get_and_symbol ctxt.alph in
-  let top = mk_fun (Alph.get_top_symbol ctxt.alph) [] in
-  let conj_cs = List.fold_left (fun a b -> mk_fun conj [a; b]) top cs in
-  let not_logical = Term.check_logical_term ctxt.alph conj_cs <> None in
-  (*Format.printf " %s\n  logical? %i %!\n" (P.to_string_term conj_cs)
-    (if not_logical then 0 else 1);*)
-  if not_logical then false
-  else (
-     let r = Smt.Solver.satisfiable [conj_cs] (smt ()) ctxt.env in
-     (*Format.printf "  sat ? %i\n%!" (if r then 1 else 0);*)
-     r)
-;;
-
-(* Do forward narrowing from last terms in sequences, trying all possible
-   rules and positions but eliminating sequences that exceed the bounds. *)
-let all_forward c rules _ seqs =
-  let cs (rl,_,tau) = L.map (Sub.apply_term tau) (Rule.constraints rl) in
-  let useful seq = small c seq && short c seq && (constr_sat c (cs seq)) in
-  let seqs' = LL.filter useful seqs in
-  let seqs'' = LL.concat (LL.map (forward c rules) seqs') in
-  if LL.null seqs'' then None else Some seqs''
 ;;
 
 let conjunction ctxt =
@@ -169,13 +105,28 @@ let refined_subst_constr_valid ctxt cs sigma =
   r
 ;;
 
+(* Check whether constraints cs are satisfiable. *)
+let constr_sat ctxt cs =
+  let mk_fun = Term.make_function ctxt.alph ctxt.env in
+  let conj = Alph.get_and_symbol ctxt.alph in
+  let top = mk_fun (Alph.get_top_symbol ctxt.alph) [] in
+  let conj_cs = List.fold_left (fun a b -> mk_fun conj [a; b]) top cs in
+  let not_logical = Term.check_logical_term ctxt.alph conj_cs <> None in
+  (*Format.printf " %s\n  logical? %i %!\n" (P.to_string_term conj_cs)
+    (if not_logical then 0 else 1);*)
+  if not_logical then false
+  else (
+     let r = Smt.Solver.satisfiable [conj_cs] (smt ()) ctxt.env in
+     (*Format.printf "  sat ? %i\n%!" (if r then 1 else 0);*)
+     r)
+;;
+
 (* Check whether the given rewrite sequence constitutes a loop; to that end
    it is checked whether the initial term unifies with (a subterm of) the final
    term, and constraint conditions are satisfied. *)
 let check ctxt ((rule, rs, sigma) as seq) =
   let (s, t) = Rule.to_terms rule in
   let cs = Rule.constraints rule in
-
   let check' get_subst t' b =
     try
       let tau = get_subst t' s in
@@ -188,7 +139,6 @@ let check ctxt ((rule, rs, sigma) as seq) =
       else None
     with Elogic.Not_unifiable | Elogic.Not_matchable -> None
   in
-
   if not (constr_sat ctxt cs) then LL.empty
   else
     let check t' =
@@ -198,6 +148,56 @@ let check ctxt ((rule, rs, sigma) as seq) =
           | Some loop -> [loop]
           | None -> [])
     in LL.of_list (L.flat_map check (Term.subterms t))
+;;
+
+(* Shorthand to rename a rule. *)
+let rename_rule c rule =
+  let fn = Alph.fun_names c.alph in
+  let newenv = Environment.empty 10 in
+  Rule.rename rule c.env
+;;
+
+(* Narrow last term in sequence using given rule at position p. *)
+let narrow c ((st, rs,sigma) as seq) p rule =
+  let rule' = rename_rule c rule in
+  let l,r = Rule.lhs rule', Rule.rhs rule' in
+  try
+    let s,t = Rule.lhs st, Rule.rhs st in
+    let mgu = Elogic.unify (Term.subterm p t) l in
+    let constr = Rule.constraints st @ (Rule.constraints rule') in
+    let st' = Rule.apply_sub mgu (Rule.create s (Term.replace p r t) constr) in
+    let sigma' = Sub.compose Sub.apply_term sigma mgu in
+    [st', rs @ [(rule, p)], sigma']
+  with Elogic.Not_unifiable -> []
+;;
+
+(* Do forward narrowing using rules in trs from last terms in sequence, trying
+   all possible rules and positions. *)
+let forward c (rules_root, rules_below) ((st,rs,_) as seq) =
+  let rlps_root = List.map (Util.Pair.make Pos.root) rules_root in
+  let ps = L.remove Pos.root (Term.funs_pos (Rule.rhs st)) in
+  let rlps = rlps_root @ (L.product ps rules_below) in
+  (* do not process already found loops further *)
+  if not (LL.null (check c seq)) then LL.empty
+  else LL.of_list (L.flat_map (fun (p,rl) -> narrow c seq p rl) rlps)
+;;
+
+(* Checking whether the sequence does not exceed the maximal term size. *)
+let small c (st,_,_) =
+  Term.size (Rule.lhs st) <= c.max_size && Term.size (Rule.lhs st) <= c.max_size
+;;
+
+(* Checking whether the sequence does not exceed the maximal length. *)
+let short c (_,rs,_) = List.length rs < c.max_length
+
+(* Do forward narrowing from last terms in sequences, trying all possible
+   rules and positions but eliminating sequences that exceed the bounds. *)
+let all_forward c rules _ seqs =
+  let cs (rl,_,tau) = L.map (Sub.apply_term tau) (Rule.constraints rl) in
+  let useful seq = small c seq && short c seq && (constr_sat c (cs seq)) in
+  let seqs' = LL.filter useful seqs in
+  let seqs'' = LL.concat (LL.map (forward c rules) seqs') in
+  if LL.null seqs'' then None else Some seqs''
 ;;
 
 (* The initial sequence, starting from a rule. *)
@@ -220,17 +220,17 @@ let process verbose prob =
   let rules = Dpproblem.get_rules prob in
   let alph = Dpproblem.get_alphabet prob in
   let env = Dpproblem.get_environment prob in
-  let ctxt = mk_ctxt alph env 2 25 in
+  let ctxt = mk_ctxt alph env 3 25 in
   let loops = generate_loops ctxt dps (all_forward ctxt (dps, rules)) in
-  if LL.null loops then 
+  if LL.null loops then
     None
   else (
-    let rec print xs =
+    let rec print i xs =
       if not (LL.null xs) then
       let s = explanation (LL.hd xs) in
-      Format.printf "%s\n" s;
-      print (LL.tl xs)
-    in (*print loops;*)
+      Format.printf "%d. %s\n%!" i s;
+      print (i+1) (LL.tl xs)
+    in print 0 loops;
     Some ([ ],  explanation (LL.hd loops)))
 ;;
 
