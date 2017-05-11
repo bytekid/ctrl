2403843a59ec7bc0f7acbf11ae737d5cf18d2c44
Sarah Winkler <sarah.winkler@uibk.ac.at>
2017-05-11 14:35:56 +0200

    fix bug in refine; change printing

--- src/termination/src/loop.ml
+++ src/termination/src/loop.ml
@@ -38,6 +38,9 @@ type t = {
   max_size: int
 }
 
+(* loop candidate *)
+type seq = Rule.t * (Rule.t * Pos.t * Term.t) list * Sub.t
+
 (* Create a context record. *)
 let mk_ctxt a e ml ms = { alph = a; env = e; max_length = ml; max_size = ms } 
 
@@ -46,10 +49,14 @@ let smt () = Rewriter.smt_solver (Rewriter.get_current ());;
 
 (* String representation of loop. *)
 let sequence_to_string (st, rs,_) =
-  let add_step (str,t) (rule,p) =
-    let u = Rule.rewrite t p rule in
-    let str = str ^ " -> " ^ (P.to_string_term u) ^ "\n" in
+  let add_step (str,t) (rule,p, u) =
+  try
+    let str =
+      str ^ " -> " ^ (P.to_string_term u) ^ " [" ^(P.to_string_rule rule) ^"]\n"
+    in
     (str,u)
+  with Elogic.Not_matchable -> failwith ((P.to_string_term t) ^ " with " ^
+   (P.to_string_rule rule) ^ " causes match failure in sequence reconstruction")
   in
   let sstr = P.to_string_term (Rule.lhs st) in
   fst (List.fold_left add_step (sstr ^ " -> \n", Rule.lhs st) rs)
@@ -60,7 +67,7 @@ let constraints_to_string = function
     [] -> ""
   | [c] -> P.to_string_term c
   | c :: cs ->
-    let add_constr str c = (P.to_string_term c) ^ " /\\ " str in
+    let add_constr str c = (P.to_string_term c) ^ " /\\ " ^ str in
     List.fold_left add_constr (P.to_string_term c) cs
 ;;
 
@@ -68,8 +75,14 @@ let constraints_to_string = function
 let explanation ((rl,_,_) as loop) =
   let cs = Rule.constraints rl in
   "We use the loop processor to conclude nontermination.\n" ^
-  "The loop is given by the sequence \n" ^ (sequence_to_string loop) ^ "\n" ^
-  (if cs = [] then "" else " with constraints " ^ (constraints_to_string cs))
+  "The loop is given by the sequence \n" ^ (sequence_to_string loop) ^
+  (if cs = [] then "" else " if " ^ (constraints_to_string cs) ^ "\n" )
+;;
+
+let substr sigma =
+  let xstr x = P.to_string_term (Term.make_var x) in
+  let app x t s = " " ^ (xstr x) ^ " -> " ^ (P.to_string_term t) ^ "\n" ^ s in
+  "{" ^ (Sub.fold app sigma "}")
 ;;
 
 let conjunction ctxt =
@@ -81,28 +94,40 @@ let conjunction ctxt =
     | c :: cs -> List.fold_left (fun a b -> mk_fun conj [a; b]) c cs
 ;;
 
+let logical_check check ctxt f =
+  let not_logical = Term.check_logical_term ctxt.alph f <> None in
+  if not_logical then false
+  else check [f] (smt ()) ctxt.env
+;;
+
+let logical_sat = logical_check Smt.Solver.satisfiable
+
+let logical_valid = logical_check Smt.Solver.valid
+
 (* Given constraints cs, check whether cs => cs sigma is valid. *)
 let subst_constr_valid ctxt cs sigma =
-  (*Sub.iter (fun x t -> Format.printf " %s -> %s\n"
-    (P.to_string_term (Term.make_var x)) (P.to_string_term t)) sigma;*)
   let mk_fun = Term.make_function ctxt.alph ctxt.env in
   let disj = Alph.get_or_symbol ctxt.alph in
   let neg = Alph.get_not_symbol ctxt.alph in
   let c1 = conjunction ctxt cs in
   let c2 = Sub.apply_term sigma c1 in
   let c = mk_fun disj [mk_fun neg [c1]; c2] in
-  let r = Smt.Solver.valid [c] (smt ()) ctxt.env in
+  let r = logical_valid ctxt c in
   r
 ;;
+
 
 let refined_subst_constr_valid ctxt cs sigma =
   let mk_fun = Term.make_function ctxt.alph ctxt.env in
   let eq = Alph.get_equal_symbol ctxt.alph in
   let app x t cs = (mk_fun eq [Term.make_var x; t]) :: cs in
-  let c = conjunction ctxt (Sub.fold app sigma []) in
-  let c_cap = Term.logical_cap ctxt.alph ctxt.env c in
-  let r = Smt.Solver.satisfiable [c_cap] (smt ()) ctxt.env in
-  r
+  let c = conjunction ctxt (Sub.fold app sigma cs) in
+  (*let c_cap = Term.logical_cap ctxt.alph ctxt.env c in*)
+  let not_logical = Term.check_logical_term ctxt.alph c <> None in
+  if not_logical then None
+  else (
+    let r = Smt.Solver.satisfiable_formulas [c] (smt ()) ctxt.env in
+    if fst r = SAT then Some (snd r) else None)
 ;;
 
 (* Check whether constraints cs are satisfiable. *)
@@ -111,14 +136,7 @@ let constr_sat ctxt cs =
   let conj = Alph.get_and_symbol ctxt.alph in
   let top = mk_fun (Alph.get_top_symbol ctxt.alph) [] in
   let conj_cs = List.fold_left (fun a b -> mk_fun conj [a; b]) top cs in
-  let not_logical = Term.check_logical_term ctxt.alph conj_cs <> None in
-  (*Format.printf " %s\n  logical? %i %!\n" (P.to_string_term conj_cs)
-    (if not_logical then 0 else 1);*)
-  if not_logical then false
-  else (
-     let r = Smt.Solver.satisfiable [conj_cs] (smt ()) ctxt.env in
-     (*Format.printf "  sat ? %i\n%!" (if r then 1 else 0);*)
-     r)
+  logical_sat ctxt conj_cs
 ;;
 
 (* Check whether the given rewrite sequence constitutes a loop; to that end
@@ -134,9 +152,13 @@ let check ctxt ((rule, rs, sigma) as seq) =
       let rule' = if b then Rule.apply_sub tau rule else rule in
       if subst_constr_valid ctxt cs sigma' then
         Some (rule', rs, sigma')
-      else if refined_subst_constr_valid ctxt cs sigma' then
-        Some (rule', rs, sigma') (* FIXME fix substitution *)
-      else None
+      else (
+        match refined_subst_constr_valid ctxt cs sigma' with
+          | None -> None
+          | Some rho ->
+            let rule'' = Rule.apply_sub rho rule' in
+        (
+         Some (rule'', rs, Sub.compose Sub.apply_term sigma' rho)))
     with Elogic.Not_unifiable | Elogic.Not_matchable -> None
   in
   if not (constr_sat ctxt cs) then LL.empty
@@ -167,14 +189,19 @@ let narrow c ((st, rs,sigma) as seq) p rule =
     let constr = Rule.constraints st @ (Rule.constraints rule') in
     let st' = Rule.apply_sub mgu (Rule.create s (Term.replace p r t) constr) in
     let sigma' = Sub.compose Sub.apply_term sigma mgu in
-    [st', rs @ [(rule, p)], sigma']
+    [st', rs @ [(rule, p, Rule.rhs st')], sigma']
   with Elogic.Not_unifiable -> []
 ;;
 
 (* Do forward narrowing using rules in trs from last terms in sequence, trying
-   all possible rules and positions. *)
+   possible rules and positions. For the rules to be applied at the root
+   (typically dependency pairs) only consider rules which are smaller or equal
+   to the first rule (with respect to some arbitrary order), to avoid redundant
+   representations of same loop that start at different term. *)
 let forward c (rules_root, rules_below) ((st,rs,_) as seq) =
-  let rlps_root = List.map (Util.Pair.make Pos.root) rules_root in
+  let r0,_,_ = L.nth rs 0 in
+  let rsrt = L.filter (fun r -> Rule.compare r r0 <= 0) rules_root in
+  let rlps_root = List.map (Util.Pair.make Pos.root) rsrt in
   let ps = L.remove Pos.root (Term.funs_pos (Rule.rhs st)) in
   let rlps = rlps_root @ (L.product ps rules_below) in
   (* do not process already found loops further *)
@@ -201,7 +228,7 @@ let all_forward c rules _ seqs =
 ;;
 
 (* The initial sequence, starting from a rule. *)
-let init_seq rule = (rule, [rule, Position.root], Sub.empty)
+let init_seq rule = (rule, [rule, Position.root, Rule.rhs rule], Sub.empty)
 
 (* Generates loops, starting from rule set rules and using the step function
    to extend sequences.
@@ -215,15 +242,13 @@ let generate_loops ctxt start_rules step =
 
 (* Main functionality *)
 let process verbose prob =
-  (*Format.printf "Go looping!\n%!";*)
   let dps = Dpproblem.get_dps prob in
   let rules = Dpproblem.get_rules prob in
   let alph = Dpproblem.get_alphabet prob in
   let env = Dpproblem.get_environment prob in
-  let ctxt = mk_ctxt alph env 3 25 in
+  let ctxt = mk_ctxt alph env 4 25 in
   let loops = generate_loops ctxt dps (all_forward ctxt (dps, rules)) in
-  if LL.null loops then
-    None
+  if LL.null loops then None
   else (
     let rec print i xs =
       if not (LL.null xs) then
